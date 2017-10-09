8bd532883dae8a4f2faae560f844f65cc7ce8db9
James Turello <jturello@yahoo.com>
2017-10-08 17:26:48 -0700

    add proptypes

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import Ticket from "./Ticket";
 
 function TicketList (props) {
+
   return (
-    <Ticket />
+    <Ticket 
+      location="3a"
+      names="Thato and Haley"
+      issue="Firebase won't save record"
+    />
   );
 }
 
